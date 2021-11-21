5e867e63f4c26dfdc78eb8e3f42b4a0c2e5feb66
dongyeopca <ckehdduq95@gmail.com>
2021-11-21 19:06:51 +0900

    message service

--- myunit/chat/urls.py
+++ myunit/chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('messages/', views.message_list, name='message-detail'),
+    path('messages/', views.Message_Post, name='message-detail'),
+    path('messages/<int:sender>/<int:receiver>',views.Message_Get)
 ]
--- myunit/chat/views.py
+++ myunit/chat/views.py
@@ -1,30 +1,33 @@
 from django.http.response import JsonResponse
+from django.views.decorators import csrf
 from django.views.decorators.csrf import csrf_exempt
 from chat.models import Message 
 from chat.serializers import MessageSerializer # Our Serializer Classes
-from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
+from django.db.models import Q
 import json
+
 
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-def message_list(request):
-    if request.method == 'GET':
-        data = json.loads(request.body)
-        messages = Message.objects.filter(sender=data["sender"], receiver=data["receiver"])
-        serializer = MessageSerializer(messages, many=True, context={'request': request})
+@api_view(['POST'])
+@csrf_exempt
+def Message_Post(request):
+    data = json.loads(request.body)
+    serializer = MessageSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
-        for message in messages:
-            message.is_read = True
-            message.save()
-        return JsonResponse(serializer.data, safe=False)
+@api_view(['GET'])
+def Message_Get(request,sender,receiver):
+    messages = Message.objects.filter(Q(sender=sender,receiver=receiver) | Q(sender=receiver,receiver=sender))
+    serializer = MessageSerializer(messages, many=True, context={'request': request})
 
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        serializer = MessageSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
+    for message in messages:
+        message.is_read = True
+        message.save()
+    return JsonResponse(serializer.data, safe=False)
